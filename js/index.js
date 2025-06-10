a90e005437e3adb6668e483c9a067111d9464fbb
Levandovskiy <levandovskiy.bogdan.i@gmail.com>
2025-06-10 23:34:50 +0300

    123

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-document.querySelector(".header__toggle").addEventListener("click", () => {
+document.querySelector(".menu__burger").addEventListener("click", () => {
   document
     .querySelector(".header__nav")
     .classList.toggle("header__nav--active");
